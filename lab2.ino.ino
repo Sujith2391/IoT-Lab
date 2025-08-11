b766f60254bf17e4ab3fa9eb223a18352d7953c8
Sujithh <sujith.22cs165@sode-edu.in>
2025-08-11 10:23:40 +0530

    Add files via upload

---
+++ lab2.ino.ino
@@ -0,0 +1,30 @@
+const int ledPins[] = {1, 2, 3, 4, 5};
+const int numLeds = 5;
+
+void setup() {
+  for(int i = 0; i < numLeds; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+
+  for(int i = 0; i < numLeds; i++) {
+    if((i + 1) % 2 == 0) {
+      digitalWrite(ledPins[i], HIGH);
+      delay(1000);
+      digitalWrite(ledPins[i], LOW);
+      delay(1000);
+    }
+  }
+
+  
+  for(int i = 0; i < numLeds; i++) {
+    if((i + 1) % 2 != 0) { 
+      digitalWrite(ledPins[i], HIGH);
+      delay(1000);
+      digitalWrite(ledPins[i], LOW);
+      delay(1000);
+    }
+  }
+}
